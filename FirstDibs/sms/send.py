ab9a035e76ebfc7cf2cd065cac5fe61b1af56094
Christopher Villalta <christophervillalta@gmail.com>
2018-02-08 20:12:31 -0500

    Remove some data

--- FirstDibs/sms/send.py
+++ FirstDibs/sms/send.py
@@ -1,10 +1,7 @@
 from twilio.rest import Client
 
 def sendMessage(numbers, message, imageurl=None):
-    # ACCOUNT_SID = "AC0ca67552c700a66330c9e395429584ff"
-    # AUTH_TOKEN = "e5272a4a070a49fa8f39d8ce5b739ed1"
-    ACCOUNT_SID = "AC1d80775323ef3be02e7c16c3193e8a95"
-    AUTH_TOKEN = "4fb9074851b5d991089dee85aad3ec07"
+# need to define ACCOUNT_SID AND AUTH_TOKEN
     # twilioNumber = "+12153020189"
     twilioNumber = "+12153302374"
     client = Client(ACCOUNT_SID, AUTH_TOKEN)
